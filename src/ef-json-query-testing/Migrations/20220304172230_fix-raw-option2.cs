4bba3f7aa307ab67eb4f30ca5435457425e551b0
Brittany Fisher <bfisher@iowacomputergurus.com>
2022-03-04 11:57:35 -0600

    migration i forgot and poorly named.

---
+++ src/ef-json-query-testing/Migrations/20220304172230_fix-raw-option2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ef_json_query_testing
+{
+    public partial class fixrawoption2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JsonDocument",
+                table: "Media_Json");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JsonDocument",
+                table: "Media_Json",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

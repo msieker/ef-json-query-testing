3012b127556941c37a3cea6a40ed1536dfadf69b
Brittany Fisher <bfisher@iowacomputergurus.com>
2022-03-10 11:12:38 -0600

    add migration for index

---
+++ src/ef-json-query-testing/Migrations/20220310143158_addIndex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ef_json_query_testing
+{
+    public partial class addIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DynamicMediaInformation_FieldId",
+                table: "DynamicMediaInformation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DynamicMediaInformation_FieldId",
+                table: "DynamicMediaInformation",
+                column: "FieldId")
+                .Annotation("SqlServer:Include", new[] { "Value", "MediaId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DynamicMediaInformation_FieldId",
+                table: "DynamicMediaInformation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DynamicMediaInformation_FieldId",
+                table: "DynamicMediaInformation",
+                column: "FieldId");
+        }
+    }
+}

dde6214fe156abb0a9d3bf2fbfee73902ca53736
Brittany Fisher <bfisher@iowacomputergurus.com>
2022-06-22 14:04:13 -0500

    add migration to remove unused column

---
+++ src/ef-json-query-testing/Migrations/20220622185412_remove-json-name-column.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ef_json_query_testing
+{
+    public partial class removejsonnamecolumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JsonName",
+                table: "DynamicFields");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JsonName",
+                table: "DynamicFields",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

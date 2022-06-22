cffda51355413550abb3b6976fcc35e2e4903274
Brittany Fisher <bfisher@iowacomputergurus.com>
2022-06-22 14:05:33 -0500

    alter json index stp to properly format dates

--- src/ef-json-query-testing/Migrations/20220622185925_Alter_stp_Add_Json_Index.Designer.cs
+++ src/ef-json-query-testing/Migrations/20220622185925_Alter_stp_Add_Json_Index.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ef_json_query_testing
 {
     [DbContext(typeof(EfTestDbContext))]
-    partial class EfTestDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220622185925_Alter_stp_Add_Json_Index")]
+    partial class Alter_stp_Add_Json_Index
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ef-json-query-testing/Migrations/20220622185925_Alter_stp_Add_Json_Index.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ef_json_query_testing
+{
+    public partial class Alter_stp_Add_Json_Index : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFile = Path.Combine("Sql/Alter_v1_stp_add_json_index.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sqlFile = Path.Combine("Sql/Undo_Alter_v1_stp_add_json_index.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+    }
+}
